d18e88ba38490bee3b3d07bd4584214ccabb6cf2
saxsir <saxsir.256+github@gmail.com>
2015-05-06 14:48:53 +0900

    [wip] layout_pattern_id と capture_image_pathのNULL制約を解除
    - とりあえずURLだけ登録して、裏でキャプチャーするスクリプトを回す形にしたいので、
    　データ登録時はNULLで入るため

--- db/migrate/20150506054355_change_column_to_web_page.rb
+++ db/migrate/20150506054355_change_column_to_web_page.rb
@@ -1,4 +1,11 @@
 class ChangeColumnToWebPage < ActiveRecord::Migration
-  def change
-  end
+    def up
+        change_column :web_pages, :capture_image_path, :string, null: true
+        change_column :web_pages, :layout_pattern_id, :integer, null: true
+    end
+
+    def down
+        change_column :web_pages, :capture_image_path, :string, null: false
+        change_column :web_pages, :layout_pattern_id, :integer, null: false
+    end
 end
