f0455051e1253db43ae74b964d79d0f2080de1c9
saxsir <saxsir.256+github@gmail.com>
2015-05-06 16:42:25 +0900

    複数のURLを一括で登録するスクリプトを書いた
    - $ bin/rails runner HelperScript.insert_multiple_urls ./urls.txt.sample
    - DBでユニーク制約がついているので同じURLは登録されない
    - 別のスクリプトでスクレイプしてきたURLをまとめて登録する用途を想定

---
+++ lib/helper_script.rb
@@ -0,0 +1,20 @@
+class HelperScript
+  def self.insert_multiple_urls
+    file_path = ARGV[0]
+
+    #TODO: ファイル名（引数）がなかったらコマンドの使用例を表示する
+    File.open(file_path) do |f|
+      f.each_line do |line|
+        web_page = WebPage.new(:url => line)
+
+        begin
+          web_page.save
+        rescue => err
+          puts err.message
+        ensure
+          puts line
+        end
+      end
+    end
+  end
+end
