502e2f93a969dce847c522da891cd51c340874d7
saxsir <saxsir.256+github@gmail.com>
2015-05-11 14:29:30 +0900

    [clean] ネストが深くならないようにif文の条件を変更

--- bin/getData.js
+++ bin/getData.js
@@ -9,31 +9,29 @@ page.viewportSize = {
 };
 
 page.open(url, function(status) {
-  if(status === "success") {
-    // e.g. http://example.com/hoge/moge/index.html
-    var basePath = '/tmp/asm-images/';
-    var saveDirPath = url.replace(/^http(s)?:\/\//, '')    // 'example.com/hoge/moge/index.html'
-        .split('/')    // ['example.com', 'hoge', 'moge', 'index.html']
-        .slice(0, -1)    // ['example.com', 'hoge', 'moge']
-        .join('/');    // 'example.com/hoge/moge
-
-    // gyazoにアップロードするまでの一時的な保存場所
-    var tmpImageFilePath = basePath + saveDirPath + '/original.png';
-    page.render(tmpImageFilePath);
-
-    // TODO: レイアウト情報を取得してJSONで吐き出す
-    var res = {
-        status: status,
-        image_file_path: tmpImageFilePath
-    };
-    console.log(JSON.stringify(res));    // rubyが受け取る返り値
-  } else {
-    // ページを開けなかった場合はstatusだけ返す
-    var errResponse = {
-        status: status
-    };
+  if (status !== 'success') {
+    var errResponse = {status: status};
     console.log(JSON.stringify(errResponse));
+    phantom.exit();
   }
+
+  // e.g. http://example.com/hoge/moge/index.html
+  var basePath = '/tmp/asm-images/';
+  var saveDirPath = url.replace(/^http(s)?:\/\//, '')    // 'example.com/hoge/moge/index.html'
+      .split('/')    // ['example.com', 'hoge', 'moge', 'index.html']
+      .slice(0, -1)    // ['example.com', 'hoge', 'moge']
+      .join('/');    // 'example.com/hoge/moge
+
+  // gyazoにアップロードするまでの一時的な保存場所
+  var tmpImageFilePath = basePath + saveDirPath + '/original.png';
+  page.render(tmpImageFilePath);
+
+  // TODO: レイアウト情報を取得してJSONで吐き出す
+  var res = {
+      status: status,
+      image_file_path: tmpImageFilePath
+  };
+  console.log(JSON.stringify(res));    // rubyが受け取る返り値
 
   phantom.exit();
 });
