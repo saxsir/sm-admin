045d7bce76eeb707791815c80e4186664d86117f
saxsir <saxsir.256+github@gmail.com>
2015-05-25 14:20:40 +0900

    [wip] wikipediaからランダムにページタイトルを抽出する関数を書いた
    - refs #24

--- lib/random_sampler.rb
+++ lib/random_sampler.rb
@@ -1,7 +1,11 @@
 class RandomSampler
   # wikipediaからランダムな単語を取得する
   def self.get_random_keyword
-    puts `curl http://ja.wikipedia.org/wiki/Special:Randompage -sL G \<title\>`
+    # htmlの<title>タグで囲まれた文字列を抽出
+    `curl -sL http://ja.wikipedia.org/wiki/Special:Randompage`.match(/<title>([^<]+)<\/title>/)
+
+    title = $1.split(' - ').first
+    return title
   end
 
   # 受け取ったクエリでgoogle検索し、上位10件のURLを取得する
