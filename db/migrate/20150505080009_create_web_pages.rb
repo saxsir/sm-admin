b650fbfefd875decbfe82d177371e8a13c9a16f9
saxsir <saxsir.256+github@gmail.com>
2015-05-25 14:27:52 +0900

    [change] WebPageモデルに検索時のクエリカラムを追加
    - refs #24
    - クエリは絶対にあるはずなのでnot null制約

--- db/migrate/20150505080009_create_web_pages.rb
+++ db/migrate/20150505080009_create_web_pages.rb
@@ -2,6 +2,7 @@ class CreateWebPages < ActiveRecord::Migration
   def change
     create_table :web_pages do |t|
       t.string :url, :null => false
+      t.string :query, :null => false
       t.string :capture_image_path, :null => false
       t.integer :layout_pattern_id, :null => false
       t.text :note
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150511073419) do
 
   create_table "web_pages", force: :cascade do |t|
     t.string   "url",                  null: false
+    t.string   "query",                null: false
     t.string   "capture_image_path"
     t.integer  "layout_pattern_id"
     t.text     "note"
