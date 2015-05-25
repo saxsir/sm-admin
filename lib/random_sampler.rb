ddfc5e523becccac4a54a308dfceebaccc6d39f5
saxsir <saxsir.256+github@gmail.com>
2015-05-25 14:21:34 +0900

    [wip] 受け取ったクエリでgoogle検索し、上位10件のURLを取得する関数を書いた
    - refs #24

--- lib/random_sampler.rb
+++ lib/random_sampler.rb
@@ -1,3 +1,4 @@
+require 'json'
 class RandomSampler
   # wikipediaからランダムな単語を取得する
   def self.get_random_keyword
@@ -10,5 +11,16 @@ class RandomSampler
 
   # 受け取ったクエリでgoogle検索し、上位10件のURLを取得する
   def self.fetch_urls(query)
+    json = `node lib/fetchUrls.js #{query}`
+
+    begin
+      urls = JSON.parse(json)
+    rescue => err
+      puts "[Search Error] Could not parse return value json"
+      urls = []
+    end
+
+    return urls
+  end
   end
 end
